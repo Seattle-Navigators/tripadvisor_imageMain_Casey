071d44a6c97ae252574ae003ae153b940069d71f
Casey Jackson <ctjacks3@gmail.com>
2020-06-19 12:34:21 -0700

    this commit will fix pomander issues

--- server/index.js
+++ server/index.js
@@ -1,9 +1,9 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
 
-app.use(express.static('public'))
-app.get('/',(req,res)=>res.send('hello world!'))
+const app = express();
+const port = 3000;
 
+app.use(express.static('public'));
+app.get(' /api/carousel/:id', (req, res) => res.send('hello world!'));
 
-app.listen(port,()=>console.log(`server listening to locolhost${port}`))
+app.listen(port, () => console.log(`server listening to locolhost${port}`));
